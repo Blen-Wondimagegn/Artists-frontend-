403f2d313ee43f7c03a3de82a152129d691bc2fd
blen_meku <mekuriablen@gmail.com>
2020-12-11 21:37:53 -0800

    bulit a constructor

---
+++ models/artist.js
@@ -0,0 +1,11 @@
+class Artist {
+    constructor(id, name, gender, age, height) {
+        this.id = id
+        this.name = name
+        this.gender = gender
+        this.age = age
+        this.height = height
+        // this.image = image
+        this.renderArtist()
+    }
+}
