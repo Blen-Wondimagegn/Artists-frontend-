5433fc62caa53b1bba168aa67deee41a0be1723c
blen_meku <mekuriablen@gmail.com>
2020-12-16 21:33:04 -0800

    modfied renderGlam and deleteGlam method

--- models/glam.js
+++ models/glam.js
@@ -12,7 +12,7 @@ class Glam {
 
     renderGlam() {
         //  debugger
-        const aHolder = document.getElementById(`${this.artist_id}`)
+        const aHolder = document.getElementById(`artist-${this.artist_id}`)
         const aContainer = document.createElement('div')
         aContainer.dataset.id = this.id
         aContainer.id = this.id
@@ -40,34 +40,10 @@ class Glam {
    </div>`
     }
 
-    // commentHTML() {
-    //     return `
-    //        <h3>Reviews</h3>
-    //         <div id='list' class='comments'></div>
-    //         <p>Leave a review </p>
-    //         <form id="comment-form" action="create">
-    //             <input type='text' name="likes" id="comment-input" cols="30" rows="10">
-    //             </br>
-    //             <button id='submit'>submit</button>
-    //         </form> `
-    // }
-    // renderComment() {
-    //     const commentHolder = document.getElementById('list')
-    //     const commentContainer = document.createElement('li')
-    //     commentContainer.dataset.id = this.id
-    //     commentContainer.id = this.id
-    //     commentContainer.classList.add = "comments"
-    //     commentContainer.innerHTML += this.commentHTML()
-    //     commentHolder.appendChild(commentContainer)
-    //     commentContainer.addEventListener("click", e => {
-    //         if (e.target.className === "form-class") this.createComment(e)
-    //         // comment.textContent = event.target.comment.value
-    //         // comments.append(comment)
-    //     })
-    // } 
+
 deleteGlam(e) {
         //  debugger
-        const id = parseInt(e.target.parentElement.parentElement.parentElement.id)
+        const id = parseInt(e.target.parentElement.parentElement.parentElement.dataset.id)
         fetch(`http://localhost:3000/glams/${id}`, {
             method: 'DELETE',
             headers: {
@@ -76,7 +52,7 @@ deleteGlam(e) {
         })
             .then(() => {
 
-                document.getElementById('new-list').removeChild(document.getElementById(id))
+                document.getElementById(`artist-${this.artist_id}`).removeChild(document.getElementById(id))
             })
     }
 
