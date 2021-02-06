8a9171673742b4a52eb7d28a0c91da7f2b44f8ab
blen_meku <mekuriablen@gmail.com>
2021-02-06 01:50:45 -0800

    modified

--- models/glam.js
+++ models/glam.js
@@ -1,57 +1,57 @@
 class Glam {
-    constructor(id, glam_squad, makeup, hair, wardrobe, artist_id) {
-        this.id = id
-        this.glam_squad = glam_squad
-        this.makeup = makeup
-        this.hair = hair
-        this.wardrobe = wardrobe
-        this.artist_id = artist_id
-        this.renderGlam()
-    }
+  constructor(id, glam_squad, makeup, hair, wardrobe, artist_id) {
+    this.id = id;
+    this.glam_squad = glam_squad;
+    this.makeup = makeup;
+    this.hair = hair;
+    this.wardrobe = wardrobe;
+    this.artist_id = artist_id;
+    this.renderGlam();
+  }
 
-    renderGlam() {
-        //  debugger
-        const aHolder = document.getElementById(`artist-${this.artist_id}`)
-        const aContainer = document.createElement('div')
-        aContainer.dataset.id = this.id
-        aContainer.id = this.id
-        aContainer.classList.add ="artist-lists"
-        aContainer.innerHTML += this.glamHTML()
-         aHolder.appendChild(aContainer)
-        aContainer.addEventListener("click", e => {
-            if (e.target.className.includes('delete')) this.deleteGlam(e)
-        })
-    }
+  renderGlam() {
+    //  debugger
+    const aHolder = document.getElementById(`artist-${this.artist_id}`);
+    const aContainer = document.createElement("div");
+    aContainer.dataset.id = this.id;
+    aContainer.id = this.id;
+    aContainer.classList.add = "artist-lists";
+    aContainer.innerHTML += this.glamHTML();
+    aHolder.appendChild(aContainer);
+    aContainer.addEventListener("click", (e) => {
+      if (e.target.className.includes("delete")) this.deleteGlam(e);
+    });
+  }
 
-    glamHTML() {
-        // debugger 
-        return `
+  glamHTML() {
+    // debugger
+    return `
         <div class="card glam">
          <div class="card-content">
-             <span class="card-title">${this.glam_squad}</span>
-             <p>${this.makeup}</p>
-             <p>${this.hair}</p>
-             <p>${this.wardrobe}</p>
+             <span class="card-title">  Glam Squad : ${this.glam_squad}</span>
+             <p> Makeup:  ${this.makeup}</p>
+             <p> Hair:  ${this.hair}</p>
+             <p>Wardrobe:  ${this.wardrobe}</p>
              <button class="delete">DELETE</button>
              </div>
        </div>
-   </div>`
-    }
+   </div>`;
+  }
 
-
-deleteGlam(e) {
-        //  debugger
-        const id = parseInt(e.target.parentElement.parentElement.parentElement.dataset.id)
-        fetch(`http://localhost:3000/glams/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(() => {
-                document.getElementById(`artist-${this.artist_id}`).removeChild(document.getElementById(id))
-            })
-    }
-
+  deleteGlam(e) {
+    //  debugger
+    const id = parseInt(
+      e.target.parentElement.parentElement.parentElement.dataset.id
+    );
+    fetch(`http://localhost:3000/glams/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      document
+        .getElementById(`artist-${this.artist_id}`)
+        .removeChild(document.getElementById(id));
+    });
+  }
 }
-
